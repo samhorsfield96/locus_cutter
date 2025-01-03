0bc096a1eaa09160120954386bb43ed27ea9ead6
Samuel Horsfield <shorsfield@hl-codon-08-02.ebi.ac.uk>
2025-01-03 16:40:40 +0000

    Enables reading of gzipped files

--- locus_cutter.py
+++ locus_cutter.py
@@ -5,6 +5,8 @@ from Bio.Seq import Seq
 import os
 import argparse
 from collections import defaultdict
+import gzip
+from functools import partial
 
 def get_options():
     description = "Cuts out loci based on alignment of reference sequences"
@@ -16,14 +18,6 @@ def get_options():
                     help='List of file paths to cut, one per line')
     IO.add_argument('--query',
                     help='Fasta file of sequences to align and cut. Each cut will be conducted with a pair of sequences paired using the same key.')
-    IO.add_argument('--separate',
-                    action="store_true",
-                    default=False,
-                    help='Generate separate fasta files of cut and remaining sequences. ')
-    IO.add_argument('--cutonly',
-                    action="store_true",
-                    default=False,
-                    help='Only output cut sequences, ignore remaining sequence. Default = False ')
     IO.add_argument('--cutoff',
                     type=float,
                     default=0.7,
@@ -63,6 +57,13 @@ def cut_loci(infiles, seq_pair_dict, cutoff):
             file_list.append(line.strip())
 
     for file in file_list:
+        # check if FASTA is gzipped
+        gzipped = False
+        with open(file, 'rb') as test_f:
+            gzipped = True if test_f.read(2) == b'\x1f\x8b' else False
+
+        _open = partial(gzip.open, mode='rt') if gzipped == True else open
+        
         for pair_id, seq_pair in seq_pair_dict.items():
         
             best_map_pair = [None, None]
@@ -75,38 +76,39 @@ def cut_loci(infiles, seq_pair_dict, cutoff):
 
             # make sure at least one sequences found a hit
             if seq1_valid or seq2_valid:
-                fasta_sequences = SeqIO.parse(open(file), 'fasta')
-                for fasta in fasta_sequences:
-                    contig_id, sequence = fasta.id, str(fasta.seq)
-                    seq1_match = contig_id == best_map_pair[0][2]
-                    seq2_match = contig_id == best_map_pair[1][2]
-                    locus_1 = None
-                    locus_2 = None
-                    detail = ""
+                with _open(file) as handle:
+                    fasta_sequences = SeqIO.parse(handle, 'fasta')
+                    for fasta in fasta_sequences:
+                        contig_id, sequence = fasta.id, str(fasta.seq)
+                        seq1_match = contig_id == best_map_pair[0][2]
+                        seq2_match = contig_id == best_map_pair[1][2]
+                        locus_1 = None
+                        locus_2 = None
+                        detail = ""
 
-                    # both sequences match contig
-                    if seq1_match and seq2_match:
-                        # work out which way round to cut
-                        locus_1 = min(best_map_pair[0][3], best_map_pair[1][3])
-                        locus_2 = max(best_map_pair[0][4], best_map_pair[1][4])
-                        detail = "complete"
-                    # first sequence matches
-                    elif seq1_match and not seq2_match:
-                        locus_1 = best_map_pair[0][3]
-                        locus_2 = best_map_pair[0][4]
-                        detail = "1_only"
-                    # second sequence matches
-                    elif not seq1_match and seq2_match:
-                        locus_1 = best_map_pair[1][3]
-                        locus_2 = best_map_pair[1][4]
-                        detail = "2_only"
-                    
-                    # ensure match found
-                    if locus_1 != None and locus_2 != None
-                        cut = sequence[locus_1:locus_2]
+                        # both sequences match contig
+                        if seq1_match and seq2_match:
+                            # work out which way round to cut
+                            locus_1 = min(best_map_pair[0][3], best_map_pair[1][3])
+                            locus_2 = max(best_map_pair[0][4], best_map_pair[1][4])
+                            detail = "complete"
+                        # first sequence matches
+                        elif seq1_match and not seq2_match:
+                            locus_1 = best_map_pair[0][3]
+                            locus_2 = best_map_pair[0][4]
+                            detail = "1_only"
+                        # second sequence matches
+                        elif not seq1_match and seq2_match:
+                            locus_1 = best_map_pair[1][3]
+                            locus_2 = best_map_pair[1][4]
+                            detail = "2_only"
+                        
+                        # ensure match found
+                        if locus_1 != None and locus_2 != None
+                            cut = sequence[locus_1:locus_2]
 
-                        cut_records.append(SeqRecord(Seq(cut), id=contig_id + "_" + pair_id + "_" + detail,
-                                                    description=fasta.description))                        
+                            cut_records.append(SeqRecord(Seq(cut), id=contig_id + "_" + pair_id + "_" + detail,
+                                                        description=fasta.description))                        
     return cut_records
 
 def main():
@@ -116,12 +118,21 @@ def main():
     cutoff = options.cutoff
     outpref = options.outpref
     
+    # check if FASTA is gzipped
+    gzipped = False
+    with open(query, 'rb') as test_f:
+        gzipped = True if test_f.read(2) == b'\x1f\x8b' else False
+
+    _open = partial(gzip.open, mode='rt') if gzipped == True else open
+    
     # get pairs of query sequences
     seq_pair_dict = defaultdict(list)
-    fasta_sequences = SeqIO.parse(open(query), 'fasta')
-    for fasta in fasta_sequences:
-        id, sequence = fasta.id, str(fasta.seq)
-        seq_pair_dict[id].append(sequence)
+
+    with _open(query) as handle:
+        fasta_sequences = SeqIO.parse(handle, 'fasta')
+        for fasta in fasta_sequences:
+            id, sequence = fasta.id, str(fasta.seq)
+            seq_pair_dict[id].append(sequence)
     
     # check that each sequence pair only has two sequences
     for pair_id, seq_pair in seq_pair_dict.items():
