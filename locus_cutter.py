6ea4ee8ba71864e0e1275a52ed8daf77743b27c7
Samuel Horsfield <shorsfield@hl-codon-08-02.ebi.ac.uk>
2025-01-11 10:53:37 +0000

    Adds printing of partial extended sequences

--- locus_cutter.py
+++ locus_cutter.py
@@ -11,13 +11,15 @@ from functools import partial
 def get_options():
     description = "Cuts out loci based on alignment of reference sequences"
     parser = argparse.ArgumentParser(description=description,
-                                     prog='python loci_cutter.py')
+                                     prog='python locus_cutter.py')
     IO = parser.add_argument_group('Input/options.out')
     IO.add_argument('--infiles',
                     required=True,
                     help='List of file paths to cut, one per line')
     IO.add_argument('--query',
-                    help='Fasta file of sequences to align and cut. Each cut will be conducted with a pair of sequences paired using the same key.')
+                    help='Fasta file of sequences to align and cut. Each cut will be conducted with a pair of sequences paired using the same key.'
+                         'Ensure sequences are on the positive strand and are placed in the file in the same order they appear on the positive strand'
+                         'e.g. dexB -> aliA for S. pneumoniae CBL.')
     IO.add_argument('--cutoff',
                     type=float,
                     default=0.7,
@@ -31,7 +33,7 @@ def get_options():
 def get_best_map(index, pair_id, seq_index, sequence, cutoff):
     a = mp.Aligner(index, preset="asm10")
 
-    best_map = (None, None, None, 0, 0, 0)
+    best_map = (None, None, None, 0, 0, 0, 0)
 
     for hit in a.map(sequence):
         if not hit.is_primary:
@@ -43,7 +45,7 @@ def get_best_map(index, pair_id, seq_index, sequence, cutoff):
             continue
 
         if query_hit > best_map[-1]:
-            best_map = (pair_id, seq_index, hit.ctg, hit.r_st, hit.r_en, query_hit)
+            best_map = (pair_id, seq_index, hit.ctg, hit.r_st, hit.r_en, query_hit, hit.strand)
 
     return best_map
 
@@ -76,6 +78,8 @@ def cut_loci(infiles, seq_pair_dict, cutoff):
 
             # make sure at least one sequences found a hit
             if seq1_valid or seq2_valid:
+                #print(best_map_pair[0])
+                #print(best_map_pair[1])
                 with _open(file) as handle:
                     fasta_sequences = SeqIO.parse(handle, 'fasta')
                     for fasta in fasta_sequences:
@@ -89,19 +93,52 @@ def cut_loci(infiles, seq_pair_dict, cutoff):
                         # both sequences match contig
                         if seq1_match and seq2_match:
                             # work out which way round to cut
+                            strand1 = best_map_pair[0][-1]
+                            strand2 = best_map_pair[1][-1]
                             locus_1 = min(best_map_pair[0][3], best_map_pair[1][3])
                             locus_2 = max(best_map_pair[0][4], best_map_pair[1][4])
-                            detail = "complete"
+                            
+                            strand_str = "_for" if (strand1 == 1 and strand2 == 1) else "_rev"
+                            detail = "complete" + strand_str
+                        
                         # first sequence matches
                         elif seq1_match and not seq2_match:
-                            locus_1 = best_map_pair[0][3]
-                            locus_2 = best_map_pair[0][4]
-                            detail = "1_only"
+                            # if seq2_valid, means likely contig break
+                            if seq2_valid:
+                                strand = best_map_pair[0][-1]
+                                strand_str = "_for" if strand == 1 else "_rev"
+                                # positive strand, set locus_2 as end of contig
+                                if strand == 1:
+                                    locus_1 = best_map_pair[0][3]
+                                    locus_2 = len(sequence) + 1
+                                # negative strand, set locus_1 as beginning of contig
+                                else:
+                                    locus_1 = 0
+                                    locus_2 = best_map_pair[0][4]
+                                detail = "1_extended" + strand_str
+                            else:
+                                locus_1 = best_map_pair[0][3]
+                                locus_2 = best_map_pair[0][4]
+                                detail = "1_only" + strand_str
                         # second sequence matches
                         elif not seq1_match and seq2_match:
-                            locus_1 = best_map_pair[1][3]
-                            locus_2 = best_map_pair[1][4]
-                            detail = "2_only"
+                            # if seq1_valid, means likely contig break
+                            if seq1_valid:
+                                strand = best_map_pair[1][-1]
+                                strand_str = "_for" if strand == 1 else "_rev"
+                                # positive strand, set locus_1 as start of contig
+                                if strand == 1:
+                                    locus_1 = 0
+                                    locus_2 = best_map_pair[1][4]
+                                # negative strand, set locus_2 as end of contig
+                                else:
+                                    locus_1 = best_map_pair[1][3]
+                                    locus_2 = len(sequence) + 1
+                                detail = "2_extended" + strand_str
+                            else:
+                                locus_1 = best_map_pair[1][3]
+                                locus_2 = best_map_pair[1][4]
+                                detail = "2_only" + strand_str
                         
                         # ensure match found
                         if locus_1 != None and locus_2 != None:
@@ -117,6 +154,12 @@ def main():
     query = options.query
     cutoff = options.cutoff
     outpref = options.outpref
+    # order = options.order
+
+    # if order != None:
+    #     assert order == "1,2" or order == "2,1"
+    #     order = order.split(",")
+    #     order = (order[0], order[1])
     
     # check if FASTA is gzipped
     gzipped = False
